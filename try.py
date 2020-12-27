9d76d540ddb6d3f311ffc92c3c6320160bee76ce
Sahar Itzhak <sahar.itzhak@QUALITEST.LOCAL>
2020-12-27 14:03:37 +0200

    Adding new files

---
+++ try.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome("C:/Users/Sahar.itzhak/Downloads/Chromedriver/chromedriver")
+driver.implicitly_wait(10)
+driver.get("https://www.google.com/")
+assert "Google" in driver.title
+
+time.sleep(2)
+print("Success")
+driver.quit()
