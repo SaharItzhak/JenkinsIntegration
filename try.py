ec9cee10cbb579177588f7d6979acc6898039653
Xala <56874379+SaharItzhak@users.noreply.github.com>
2020-12-28 12:55:18 +0200

    Update try.py

--- try.py
+++ try.py
@@ -1,12 +1,17 @@
-from selenium import webdriver
-import time
+#from selenium import webdriver
+#import time
+#
+#driver = webdriver.Chrome("C:/Users/Sahar.itzhak/Downloads/Chromedriver/chromedriver")
+#driver.implicitly_wait(10)
+#driver.get("https://www.google.com/")
+#assert "Google" in driver.title
+#
+#time.sleep(2)
+#print("Success")
+#print("I have made some changes!-!!")
+#driver.quit()
 
-driver = webdriver.Chrome("C:/Users/Sahar.itzhak/Downloads/Chromedriver/chromedriver")
-driver.implicitly_wait(10)
-driver.get("https://www.google.com/")
-assert "Google" in driver.title
 
-time.sleep(2)
-print("Success")
-print("I have made some changes!-!!")
-driver.quit()
+print("=======================")
+print("TRY2")
+print("=======================")
